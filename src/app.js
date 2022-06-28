4237d06807269e6a0d94e3f51aadd7bb67a9da7e
amidzgz <bendhiafamid@gmail.com>
2022-06-28 10:08:41 +0000

    todos los dominios

--- src/app.js
+++ src/app.js
@@ -6,6 +6,23 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let todoslosdominios = function() {
+    for (let pro of pronoun) {
+      for (let adj1 of adj) {
+        for (let nuon of noun) {
+          dominios.push(domainname(pro, adj1, nuon));
+        }
+      }
+    }
+    return dominios;
+  };
+  var pronoun = ["the", "our"];
+  var adj = ["great", "big"];
+  var noun = ["jogger", "racoon"];
+  var dominios = [];
+  let domainname = function(pronoun, adj, noun) {
+    return `${pronoun}${adj}${noun}.com`;
+  };
+  document.querySelector(".container").innerHTML =
+    "<p class='d'>" + todoslosdominios() + "</p>";
 };
